18b7badb9406300816e45898ebc0f2c68273bfd5
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-11-17 19:31:11 +0600

    problem 1 complted

---
+++ solution.ts
@@ -0,0 +1,16 @@
+function formatValue(
+	value: string | number | boolean,
+): string | number | boolean {
+	if (typeof value === 'string') {
+		return value.toUpperCase();
+	}
+	if (typeof value === 'number') {
+		return value * 10;
+	}
+
+	return !value;
+}
+
+console.log(formatValue('hello'));
+console.log(formatValue(5));
+console.log(formatValue(true));
