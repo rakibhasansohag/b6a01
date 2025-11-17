e30e20d26893f162d537b4a1b47108403f5e9c1c
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2025-11-17 20:03:25 +0600

    problem 6  solved

--- solution.ts
+++ solution.ts
@@ -69,11 +69,38 @@ function printBookDetails(book: Book): void {
 	);
 }
 
-const myBook: Book = {
-	title: 'The Great Gatsby',
-	author: 'F. Scott Fitzgerald',
-	publishedYear: 1925,
-	isAvailable: !false,
-};
+function getUniqueValues<T extends number | string>(arr1: T[], arr2: T[]): T[] {
+	const result: T[] = [];
 
-printBookDetails(myBook);
+	for (let i = 0; i < arr1.length; i++) {
+		let exists = false;
+		for (let j = 0; j < result.length; j++) {
+			if (result[j] === arr1[i]) {
+				exists = true;
+				break;
+			}
+		}
+		if (!exists) {
+			result.push(arr1[i] as T);
+		}
+	}
+
+	for (let i = 0; i < arr2.length; i++) {
+		let exists = false;
+		for (let j = 0; j < result.length; j++) {
+			if (result[j] === arr2[i]) {
+				exists = true;
+				break;
+			}
+		}
+		if (!exists) {
+			result.push(arr2[i] as T);
+		}
+	}
+
+	return result;
+}
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [3, 4, 5, 6, 7];
+console.log(getUniqueValues(array1, array2));
